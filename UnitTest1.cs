3bf88fae2fb8f7d2797233a5cff9f285319515db
linaagafurova <91336145+linaagafurova@users.noreply.github.com>
2021-10-04 12:50:07 +0300

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,102 @@
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class UntitledTestCase
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            ChromeDriver driver1 = new ChromeDriver();
+
+            driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+            baseURL = "https://www.google.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                //
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TheUntitledTestCaseTest()
+        {
+            driver.Navigate().GoToUrl("https://store.steampowered.com/");
+            driver.FindElement(By.Id("store_nav_search_term")).Click();
+            driver.FindElement(By.Id("store_nav_search_term")).Clear();
+            driver.FindElement(By.Id("store_nav_search_term")).SendKeys("The Witcher");
+            driver.FindElement(By.XPath("//img[contains(@src,'https://store.akamai.steamstatic.com/public/images/blank.gif')]")).Click();
+            driver.FindElement(By.Id("sort_by_trigger")).Click();
+            driver.FindElement(By.Id("Price_DESC")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
